1d2eccfc515ae7c1504739821dd1a5681224dfab
carlosvfb <cacavfb2107@gmail.com>
2024-11-25 20:41:26 -0300

    first commit

---
+++ src/controllers/CreateNutritionControler.ts
@@ -0,0 +1,26 @@
+import { FastifyRequest, FastifyReply } from "fastify";
+import { CreateNutritionService } from "../services/CreateNutritionService";
+
+export interface DataProps {
+    name: string;
+    weight: string;
+    height: string;
+    age: string;
+    gender: string;
+    objective: string;
+    level: string;
+}
+
+class CreateNutritionController {
+    async handle(request: FastifyRequest, reply: FastifyReply) {
+        const { name, weight, height, age, gender, objective, level } = request.body as DataProps;
+
+        const createNutrition = new CreateNutritionService();
+
+        const nutrition = await createNutrition.execute({ name, weight, height, age, gender, objective, level });
+
+        reply.status(201).send(nutrition);
+    }
+}
+
+export { CreateNutritionController }
